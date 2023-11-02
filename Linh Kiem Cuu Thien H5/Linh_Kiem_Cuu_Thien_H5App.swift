933bdd4465d5df89445ef85c7a7764a4b4766048
Vũ Tấn Phát <phatgreen.dev@gmail.com>
2023-11-02 15:18:50 +0700

    Initial Commit

---
+++ Linh Kiem Cuu Thien H5/Linh_Kiem_Cuu_Thien_H5App.swift
@@ -0,0 +1,17 @@
+//
+//  Linh_Kiem_Cuu_Thien_H5App.swift
+//  Linh Kiem Cuu Thien H5
+//
+//  Created by Vũ Tấn Phát on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Linh_Kiem_Cuu_Thien_H5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
