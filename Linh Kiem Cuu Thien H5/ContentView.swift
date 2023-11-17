de1a444f1ef5b2501ca8c5d7f5a4e745976dcb5c
Vũ Tấn Phát <81356717+phatgreen@users.noreply.github.com>
2023-11-17 14:39:30 +0700

    17/11/2023
    Delete cache off and addd
    * {
        transform: translateZ(0);
    }

--- Linh Kiem Cuu Thien H5/ContentView.swift
+++ Linh Kiem Cuu Thien H5/ContentView.swift
@@ -9,25 +9,32 @@ import SwiftUI
 import WebKit
 
 struct ContentView: View {
-    
+
     let webView: WKWebView
-    
+
     init(homeUrl: URL) {
-        
+
         let prefs = WKWebpagePreferences()
         prefs.allowsContentJavaScript = true
+
         let config = WKWebViewConfiguration()
         config.defaultWebpagePreferences = prefs
-        
+
         self.webView = WKWebView(frame: .zero, configuration: config)
-        
+
         let request = URLRequest(url: homeUrl)
         self.webView.load(request)
+
+        // Thêm CSS bằng cách sử dụng phương thức evaluateJavaScript
+        let css = "* { -webkit-transform: translateZ(0); transform: translateZ(0); }"
+        let js = "var style = document.createElement('style'); style.innerHTML = '\(css)'; document.head.appendChild(style);"
+        self.webView.evaluateJavaScript(js, completionHandler: nil)
     }
-    
+
     var body: some View {
         VStack(spacing: 0) {
             UIViewWrapper(view: webView)
         }
     }
 }
+
